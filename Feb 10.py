7994211fa455ba2cf01bc9445b9757913725cd2d
Supramito <159730256+Supramito@users.noreply.github.com>
2025-02-10 12:40:12 -0700

    Create Feb 10.py

---
+++ Feb 10.py
@@ -0,0 +1,22 @@
+print("Hi Aryan")
+print("how da day been")
+print("(Aryan)""good")
+print("nice")
+print("""lolbot>?!@!!#""")
+print("""What could go wrong?""")
+print ("Hello Again")
+print("Please work")
+
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+
+print("Aryan")
+print("Feburary 10th 2025")
+
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling 😁")
+print("You can follow my progress at replit.com/@AryanKumar1")
