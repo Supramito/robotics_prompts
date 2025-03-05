d247fd8025209abf3af85ae2d630486882cd9466
Supramito <159730256+Supramito@users.noreply.github.com>
2025-03-05 10:15:26 -0700

    Create March 3.py

---
+++ March 3.py
@@ -0,0 +1,36 @@
+#Leanred how to use IF ELSE statements
+
+myName = input("What's your name?: ")
+if myName == "David":
+ print("Welcome Dude!")
+ print("You're just the baldest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+    print("Meow")
+else:
+    print("Woof")
+
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+
+
+drink = input("Do you prefer coffee or tea?")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
+
